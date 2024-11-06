9dce10788bf23405c49f2172a5703f622909d387
jaysprogram <107363449+jaysprogram@users.noreply.github.com>
2024-11-05 19:32:34 -0500

    DONE

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+## Reyjay Collazo
+## Pa01
+## Notes: can be greatly reduced in size if implemented more modular functions
+
 import math
 
 # Define the modulus
@@ -47,7 +51,7 @@ def calculate_result_type_1(school):
         curr_y = y_points[i]
         # Calculate the number of ways to get from (prev_x, prev_y) to (curr_x, curr_y)
         result *= choose(curr_x - prev_x + curr_y - prev_y, curr_x - prev_x)
-        result %= MOD  # Take modulo to prevent overflow
+        result %= MOD 
         prev_x, prev_y = curr_x, curr_y  # Update previous coordinates
 
     # Calculate the number of ways to get from the last point to the school
@@ -56,7 +60,43 @@ def calculate_result_type_1(school):
     
     return result
 
+def calculate_result_type_2(school): ## I can make the code more modular by making taking the input a function
+    result = 1
+    x_points = []
+    y_points = []
+    amntOfLoc = int(input())
+    
+    ## Loop x amount of time from input
+    for i in range(0,amntOfLoc):
+        inputs = input().split()
+        x_points.append(int(inputs[0]))  # Convert and append x coordinate
+        y_points.append(int(inputs[1]))
+    
+    #checks if its valid by making sure neither x or y decreases
+    if (not if_valid_locations(x_points,school[0]) or not if_valid_locations(y_points,school[1])):
+        return 0
+    schoolX = school[0]
+    schoolY = school[1]
+    ## formula1: total ways to school - [ways through bully 1 to school]
+    ## formula2: total ways to school - [ways through bully 1 to school * ways through bully 2 to school - paths through both bullies]
 
+    prev_x, prev_y = 0, 0
+    # First bully
+    result = choose(x_points[0] + y_points[0],x_points[0]) * choose(schoolX - x_points[0] + schoolY - y_points[0],schoolX - x_points[0]) % MOD
+    if len(x_points) == 1: ### the case where its only one bully
+        pass 
+    else: 
+        ## When there is two bullies
+        result += choose(x_points[1] + y_points[1],x_points[1]) * choose(schoolX - x_points[1] + schoolY - y_points[1],schoolX - x_points[1]) % MOD
+        result -= choose(x_points[0] + y_points[0],x_points[0]) * \
+            choose(x_points[1] - x_points[0] + y_points[1] - y_points[0],x_points[1] - x_points[0]) * \
+            choose(schoolX - x_points[1] + schoolY - y_points[1],schoolX - x_points[1]) % MOD 
+
+    # Calculate the number of ways to get from (0,0) to the school and subtract it 
+    result = choose(schoolX + schoolY, schoolX) - result
+    result %= MOD  # Take modulo to prevent overflow
+    
+    return result
 
 
 def main():
@@ -73,7 +113,7 @@ def main():
             print(f"{calculate_result_type_1(school)}")
         else:
             ## scenario 2 avoid bully
-            print(f"{calculate_result_type_1(school)}")
+            print(f"{calculate_result_type_2(school)}")
             pass
     
     
